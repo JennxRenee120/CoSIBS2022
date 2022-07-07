5a7126f690e1c6dfbb031e07e05852d873e3736f
Laura Jabr <lmj7@rice.edu>
2022-07-07 13:31:30 -0600

    comments

--- PCA_Visualization.R
+++ PCA_Visualization.R
@@ -23,37 +23,37 @@ new_pro <- knn_log2(pro_dat)
 
 ## PCA Visualization ##
 
-#Metabolome first two principal components
-#No scaling
+## Metabolome first two principal components
+#  No scaling
 pr.met <- prcomp(new_met)
 plot(pr.met$x[,1:2], main = "PC-1 vs PC-2 in Metabolome")
 
-#Proteome first two principal components
+## Proteome first two principal components
 #No scaling
 pr.pro <- prcomp(new_pro)
 plot(pr.pro$x[,1:2])
 
-#Metabolome first two principal components
+## Metabolome first two principal components
 #Scaled
 pr.met <- prcomp(new_met, scale. = T)
 plot(pr.met$x[,1:2])
 
-#Proteome first two principal components
+## Proteome first two principal components
 #Scaled
 pr.pro <- prcomp(new_pro, scale. = T)
 plot(pr.pro$x[,1:2])
 
-#Trying to combine proteome and metabolome
+## Trying to combine proteome and metabolome
 
-#pulling out first two PCs
+## pulling out first two PCs
 pro_pc12 <- pr.pro$x[,1:2]
 met_pc12 <- pr.met$x[,1:2]
 
-#labeling whether protein or metabolite
+## labeling whether protein or metabolite
 pro_pc12 <- cbind(pro_pc12, rep("protein", length(pro_pc12[,1])))
 met_pc12 <- cbind(met_pc12, rep("metabolite", length(met_pc12[,1])))
 
-#combining data
+## combining data
 pc12 <- rbind(pro_pc12, met_pc12)
 ggplot(data.frame(pc12), mapping = aes(x = pc12[, 1],
                                   y = pc12[, 2], col = pc12[, 3])) + 
